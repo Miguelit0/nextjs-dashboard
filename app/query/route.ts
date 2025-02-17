efbcfb7f509f759cb17c98b9eccef316c6fd5d78
Fereau Romero Miguel <fereau.romero.miguel@gmail.com>
2025-02-17 15:03:38 +0100

    Uncomment code

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,15 +3,13 @@ import postgres from "postgres";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function listInvoices() {
-  sql(`DROP TABLE invoices, customers`);
-  // const data = await sql`
-  //   SELECT invoices.amount, customers.name
-  //   FROM invoices
-  //   JOIN customers ON invoices.customer_id = customers.id
-  //   WHERE invoices.amount = 666;
-  // `;
-
-  // return data;
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+  return data;
 }
 
 export async function GET() {
